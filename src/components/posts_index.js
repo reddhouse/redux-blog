c14e05dfcaba9d60180a343fa1b8c19b5544eaed
Justin Tuttle <reddhouse@gmail.com>
2016-03-01 22:49:01 -0800

    Pass control of form to redux-form

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
-export default () => {
-	return <div>List of blog posts goes here.</div>;
-};
+class PostsIndex extends Component {
+	componentWillMount() {
+		this.props.fetchPosts();
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="text-xs-right">
+					<Link to="/posts/new" className="btn btn-primary">
+						Add a Post
+					</Link>
+				</div>
+				List of blog posts
+			</div>
+		);
+	}
+}
+
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({ fetchPosts }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps) (PostsIndex);
